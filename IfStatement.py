39c6c9876cdb09acaa37e1a185c3524276de07e9
Sabbir <54003568+SabbirAhamed007@users.noreply.github.com>
2020-08-24 13:47:01 -0400

    Initial commit

---
+++ IfStatement.py
@@ -0,0 +1,56 @@
+is_hot = True
+if is_hot:
+    print("It's a hot day")
+
+print("Enjoy the day")
+
+
+
+
+is_hot = True
+if is_hot:
+    print("It's a hot day")
+    print("Drink water")
+
+print("Enjoy the day")
+
+
+
+is_ferrari = True
+if is_ferrari:print("It's a Ferrari")
+print("it's a Italian car")
+
+
+is_day = True
+if is_day: print("Sun light is there")
+
+print("It's a sunny day")
+
+is_day = False
+if is_day: print("It's sunny")
+print("No !!! It's night")
+
+is_mercedes = False
+if is_mercedes: print("if it a mercedes")
+print("Repair is costly")
+print("Mercedes is a German car")
+
+
+is_bmw = True
+if is_bmw: print("It's a BMW")
+print("costly to repair")
+
+else: print("it's not a BMW")
+print("because it's not BMW")
+
+
+print("But the car is nice!!!")
+
+
+
+
+
+
+
+
+
